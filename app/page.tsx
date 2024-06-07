3be5da22c61c17cfda4a07587923bf7e3227ba59
Sintu Boro <sintuboro@boro.lan>
2024-06-07 13:43:11 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,92 @@
+import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image';
+import beaconLogo from 'app/img/beacon-logo.png';
+import Link from 'next/link';
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+function WorkBadge({ img, company, title, link }) {
+  return (
+    <div className="group flex w-full">
+      <Link
+        href={link}
+        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-3 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        <div className="flex items-center space-x-3">
+          <div className="relative h-16">
+            <Image
+              alt="avatar"
+              src={img}
+              height={64}
+              width={64}
+              sizes="33vw"
+              className="h-16 w-16 rounded-full object-contain bg-gray-100 border border-neutral-200 dark:border-neutral-700"
+              priority
+            />
+          </div>
+          <div className="flex flex-col">
+            <p className="font-medium text-neutral-900 dark:text-neutral-100">
+              {company}
+            </p>
+            <p className="text-sm text-gray-400">
+              {title}
+            </p>
+          </div>
+        </div>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <ArrowIcon />
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <section>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          {`Hey 👋 I'm Sintu Boro. I bring a blend of IT consulting, development, and design expertise. 
+            I have transformed complex business processes into efficient and 
+            user-friendly solutions to help create seamless digital experiences. 
+            Explore my work and discover how I bridge the gap between functionality 
+            and user-centered design.`}
+        </p>
+      </div>
+      <div className='flex items-center mt-8'>
+        <h4 className="font-medium text-xl tracking-tight">current experience</h4>
+      </div>
+      <div className="my-6 flex w-full flex-col space-x-0 space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+        <WorkBadge
+          img={beaconLogo}
+          company="Beacon Technologies, Inc."
+          title="IT Consultant/Programmer Analyst IV for Wisconsin DCF"
+          link="/work"
+        />
+      </div>
+      <div className="my-6">
+        <div className='flex items-center mt-8'>
+          <h4 className="font-medium text-xl tracking-tight">my blog</h4>
+        </div>
+        <div className="my-4">
+          <BlogPosts page='home' />
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ app/work/page.tsx
@@ -0,0 +1,119 @@
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'Work',
+  description: 'Read my work.',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+        As a developer/programmer analyst, I specialize in customizing and optimizing software platforms for 
+        improved efficiency and user experience. I focus on delivering impactful solutions that enhance 
+        both the functionality and usability of the systems I work with.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+
+        <h2 className="font-medium text-xl tracking-tighter mb-0">Beacon Technologies, Inc.</h2>
+        <div className='flex flex-row justify-between'>
+            <p className="my-2 text-sm">
+                IT Consultant/Programmer Analyst IV
+            </p>
+            <p className="my-2 text-xs">
+                <i>May, 2024 - Current</i>
+            </p>
+        </div>
+        <p>
+          I joined Beacon Technologies as a merger with Smart Solutions. While the company name might have changed, 
+          the core focus of being part of a great team and delivering robust software solutions remains the same.
+        </p>
+        <ul>
+          <li>
+            Completed POC phase, developing code for Collibra ingestion. Utilized Postman scripts for content upload, 
+            reducing upload time by 30%. Conducted preliminary testing on 10+ sample files, ensuring 99% accuracy for 
+            import readiness.
+          </li>
+          <li>
+            Implemented workflows and governance flows, translating business processes into 10+ efficient workflows 
+            within the Collibra platform.
+          </li>
+          <li>
+            Resolved 90% of production issues within a week, ensuring uninterrupted operation. Developed script for 
+            bulk deletion, reducing deletion time by 40%.
+          </li>
+          <li>
+            Streamlined asset management processes, resulting in 50% reduction in manual effort.
+          </li>
+          <li>
+            Reviewed 20 existing diagrams, implementing best practices for visualizing relationships.
+          </li>
+        </ul>
+        <p>
+            Since joining Beacon Tech I have addressed code issues, increased BO extraction efficiency by 25%, conducted 
+            comprehensive BO extractor runs on 50+ files, identified and resolved 60% of existing issues.
+        </p>
+
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+
+        <h2 className="font-medium text-xl tracking-tighter mb-0">Smart Solutions, Inc.</h2>
+        <div className='flex flex-row justify-between'>
+            <p className="my-2 text-sm">
+                IT Consultant/Programmer Analyst IV
+            </p>
+            <p className="my-2 text-xs">
+                <i>Sep, 2023 - May, 2024</i>
+            </p>
+        </div>
+        <ul>
+          <li>
+            Administered and customized the Collibra platform to meet user and agency requirements; utilized Python 
+            for automating tasks, contributing to improved functionalities.
+          </li>
+          <li>
+            Implemented workflows and governance flows, translating business processes into 10+ efficient workflows 
+            within the Collibra platform.
+          </li>
+          <li>
+            Worked extensively with Collibra platform, utilizing Java and configuring APIs to support administration.
+          </li>
+          <li>
+            Contributed to a 20% increase in user interface customization and optimization; mapped and optimized workflows,
+            resulting in a 15% improvement in business process efficiency.
+          </li>
+          <li>
+            Streamlined asset management processes, resulting in 50% reduction in manual effort.
+          </li>
+        </ul>
+
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+
+        <h2 className="font-medium text-xl tracking-tighter mb-0">Wave Drowning Prevention Systems</h2>
+        <div className='flex flex-row justify-between'>
+            <p className="my-2 text-sm">
+                UI/UX Designer and Developer
+            </p>
+            <p className="my-2 text-xs">
+                <i>May, 2022 - Sep, 2023</i>
+            </p>
+        </div>
+        <ul>
+          <li>
+            Demonstrated all stages of the UI/UX development process; created hypothetical personas, experience maps, sitemaps and utilized Figma to develop wireframes and prototypes taking user and design specifications into consideration.
+          </li>
+          <li>
+            Worked seamlessly in an Agile environment; collaborated with senior developers to represent information architectures, workflows, UI concepts and documented interaction design specifications including detailed design rationale.
+          </li>
+          <li>
+            Followed the current best practices and conventions in UX design and applied them to build effective and compelling screen-based single-page applications (SPA) for WAVE applications using React, while assisting the development team.
+          </li>
+          <li>
+            Collaborated effectively with the marketing, development teams and senior engineers to achieve quarterly KPIs.
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
