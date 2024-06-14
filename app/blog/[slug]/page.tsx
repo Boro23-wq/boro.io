d5f214e4fd9df762d8bbb286536ab8ffd64aa6c7
Sintu Boro <sintuboro@boro.lan>
2024-06-14 11:40:28 -0500

    added view count

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,22 @@
 import { notFound } from "next/navigation";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, Eye } from "lucide-react";
 import { CustomMDX } from "app/components/mdx";
 import { formatDate, getBlogPosts } from "app/blog/utils";
 import { baseUrl } from "app/sitemap";
 import Link from "next/link";
+// import { Suspense } from "react";
+import { ReportView } from "./view";
+import { Redis } from "@upstash/redis";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const redis = Redis.fromEnv();
+
+export const revalidate = 60;
 
 export async function generateStaticParams() {
   let posts = getBlogPosts();
@@ -54,8 +67,13 @@ export function generateMetadata({ params }) {
   };
 }
 
-export default async function Blog({ params }) {
+export default async function Blog({ params }: Props) {
   let posts = getBlogPosts();
+  const views =
+    (await redis.get<number>(
+      ["pageviews", "projects", params.slug].join(":")
+    )) ?? 0;
+
   const sortedPosts = posts.sort((a, b) => {
     return (
       new Date(b.metadata.publishedAt).getTime() -
@@ -97,18 +115,31 @@ export default async function Blog({ params }) {
           }),
         }}
       />
+      <ReportView slug={params.slug} />
       <h1 className="title font-semibold text-2xl tracking-tighter">
         {post.metadata.title}
       </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+      <div className="flex justify-between items-center mt-2 mb-6 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {formatDate(post.metadata.publishedAt)}
         </p>
+        <span
+          title="View counter for this page"
+          className="flex items-center gap-1 text-neutral-600 dark:text-neutral-400"
+        >
+          <Eye className="w-5 h-5" />
+          {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+        </span>
       </div>
+
+      <hr className="text-neutral-200 dark:text-neutral-700" />
+
       <article className="mb-10 prose">
         <CustomMDX source={post.content} />
       </article>
-      <hr className="text-neutral-200 dark:text-neutral-700" />
+
+      <hr className="w-60 h-1 mx-auto my-4 bg-neutral-200 border-0 rounded md:my-10 dark:bg-neutral-700" />
+
       <div className="flex justify-between mt-8">
         {previousPost ? (
           <div className="flex items-end">
