7a617804882e48585f28856d1b2dec1f90fa8f8f
Sintu Boro <sintuboro@boro.lan>
2024-06-16 16:15:42 -0500

    image update with projects mdx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,7 +17,7 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="z-10 sticky top-0 backdrop-blur-sm backdrop-saturate-100 -ml-[14px] mb-20 tracking-tight">
+    <aside className="z-10 sticky top-0 rounded-md backdrop-blur-lg backdrop-saturate-100 -ml-[14px] mb-20 tracking-tight">
       <nav
         className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
         id="nav"
